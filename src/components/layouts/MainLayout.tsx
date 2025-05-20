01f1d6b25fdbef60f60544db8266a5fe0a450ecd
Eli <elijbakal@gmail.com>
2025-05-20 19:21:17 -0400

    Remove unused headerHeight state and related code from MainLayout to fix lint error

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useLayoutEffect, useState } from "react";
+import React from "react";
 import Header from "@/components/header/Header";
 
 export type MainLayoutProps = {
@@ -14,23 +14,9 @@ export default function MainLayout({
   rightSidebar,
   children,
 }: MainLayoutProps) {
-  const headerRef = useRef<HTMLDivElement>(null);
-  const [headerHeight, setHeaderHeight] = useState(0);
-
-  useLayoutEffect(() => {
-    function updateHeight() {
-      if (headerRef.current) {
-        setHeaderHeight(headerRef.current.offsetHeight);
-      }
-    }
-    updateHeight();
-    window.addEventListener("resize", updateHeight);
-    return () => window.removeEventListener("resize", updateHeight);
-  }, []);
-
   return (
     <div className="h-screen flex flex-col">
-      <div ref={headerRef} className="fixed top-0 left-0 w-full z-50">
+      <div className="fixed top-0 left-0 w-full z-50">
         <Header variant={headerVariant} onLogout={() => {}} />
       </div>
       <div id="maincontentarea" className="flex flex-1 h-full min-h-0" style={{ marginTop: 52 }}>{/* 64px header height */}
